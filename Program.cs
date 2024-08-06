dd69b5fa133d300712ddac2c919bef2fefbe4c21
khelechy <onyekwerekelechimac@gmail.com>
2024-08-06 23:57:13 +0100

    removed non working

--- Program.cs
+++ Program.cs
@@ -7,16 +7,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// For running in Railway
-var portVar = Environment.GetEnvironmentVariable("PORT");
-if (portVar is { Length: > 0 } && int.TryParse(portVar, out int port))
-{
-    builder.WebHost.ConfigureKestrel(options =>
-    {
-        options.ListenAnyIP(port);
-    });
-}
-
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
 
