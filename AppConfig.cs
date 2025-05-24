fe86ac873cd35172c38df626c3f16b275cee5f28
khelechy <onyekwerekelechimac@gmail.com>
2025-05-24 07:43:55 +0100

    implented word rouns and start fame

--- AppConfig.cs
+++ AppConfig.cs
@@ -2,6 +2,7 @@
 {
     public static class AppConfig
     {
-        public static string BaseUrl = "https://wordlersapi.kelechi.wtf";
+       public static string BaseUrl = "https://wordlersapi.kelechi.wtf";
+        //public static string BaseUrl = "https://localhost:7000";
     }
 }
