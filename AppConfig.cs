519c6ed00412f98fdc5d66db5ef1968e82351498
khelechy <onyekwerekelechimac@gmail.com>
2025-06-03 18:14:33 +0100

    updated base

--- AppConfig.cs
+++ AppConfig.cs
@@ -2,7 +2,7 @@
 {
     public static class AppConfig
     {
-       //public static string BaseUrl = "https://wordlersapi.kelechi.wtf";
-       public static string BaseUrl = "https://localhost:7000";
+       public static string BaseUrl = "https://wordlersapi.kelechi.wtf";
+       //public static string BaseUrl = "https://localhost:7000";
     }
 }
